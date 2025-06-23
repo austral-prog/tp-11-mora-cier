091acb1757abe818a90768fd228a6585719111ba
mora-cier <mcier@mail.austral.edu.ar>
2025-06-23 18:47:52 -0300

    Update files_and_exceptions.py

--- files_and_exceptions.py
+++ files_and_exceptions.py
@@ -1,21 +1,23 @@
 def read_file_to_dict(filename):
-	ventas_dict = {}
-	with open (filename, "r") as f:
+	ventas_dict = dict()
+	with open(filename, "r") as f:
 		linea = f.readline().strip()
-		ventas = linea.split("";"")
+		ventas = linea.split(";")
 
 		for venta in ventas:
-			if ventas:
-				producto, valor = venta.split("":"")
+			if venta:
+				producto, valor = venta.split(":")
 				valor = float(valor)
 				if producto not in ventas_dict:
 					ventas_dict[producto] = []
 				ventas_dict[producto].append(valor)
+
 	return ventas_dict
 
-def process_dict():
+
+def process_dict(data):
 	for producto in sorted(data.keys()):
-		lista_ventanas = data[producto]
-		total = sum(lista_ventanas)
-		promedio = total / len(lista_ventanas)
+		lista_ventas = data[producto]
+		total = sum(lista_ventas)
+		promedio = total / len(lista_ventas)
 		print(f"{producto}: ventas totales ${total:.2f}, promedio ${promedio:.2f}")
